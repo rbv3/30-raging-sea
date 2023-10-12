4a5477941ebfe47886ce6669b0210bf7e08fa7f1
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-12 19:57:55 -0300

    not yet raging, but already sea

---
+++ src/shaders/water/vertex.glsl
@@ -0,0 +1,23 @@
+uniform float uTime;
+uniform float uBigWavesSpeed;
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+
+varying float vElevation;
+
+void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    
+    // Elevation
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + (uTime * uBigWavesSpeed)) *
+                      sin(modelPosition.z * uBigWavesFrequency.y + (uTime * uBigWavesSpeed)) *
+                      uBigWavesElevation;
+    modelPosition.y += elevation;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPosition;
+
+    vElevation = elevation;
+}
